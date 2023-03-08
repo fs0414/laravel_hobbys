ab4e325a5ff88d9f5fa3cb6606285ea5c4785338
fujitani.s <fujitani.s@impl.co.jp>
2023-03-08 23:55:43 +0900

    fix: userRegister_rules fix

--- laravel/app/Http/Requests/Auth/UserRegisterRequest.php
+++ laravel/app/Http/Requests/Auth/UserRegisterRequest.php
@@ -23,7 +23,7 @@ class UserRegisterRequest extends FormRequest
     {
         return [
             'name' => 'required | string | max:255',
-            'email' => 'required | string|email | max:255 | unique:users',
+            'email' => 'required | string | email | max:255 | unique:users',
             'password' => 'required | string | min:8',
         ];
     }
